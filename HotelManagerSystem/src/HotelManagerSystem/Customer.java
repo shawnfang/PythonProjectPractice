6b1c971f7903f80ae09fc830670bc22c7d7936c3
shawnfang <shawnfang07@gmail.com>
2020-05-19 14:00:38 +0800

    update

---
+++ HotelManagerSystem/src/HotelManagerSystem/Customer.java
@@ -0,0 +1,15 @@
+package HotelManagerSystem;
+
+public class Customer {
+    String name;
+    String customerId;
+
+    public Customer(){
+
+    }
+
+    public Customer(String name,String customerId){
+        this.customerId=customerId;
+        this.name=name;
+    }
+}
