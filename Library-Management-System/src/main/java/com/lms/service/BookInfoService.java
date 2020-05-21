2c2da0808cb72e4203e0f826942208ed8ff99a92
shawnfang <shawnfang07@gmail.com>
2020-05-21 18:23:47 +0800

    update

--- Library-Management-System/src/main/java/com/lms/service/BookInfoService.java
+++ Library-Management-System/src/main/java/com/lms/service/BookInfoService.java
@@ -1,9 +1,11 @@
 package com.lms.service;
 
 import com.lms.entity.BookInfo;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface BookInfoService {
     BookInfo getById(int bookId);
     List<BookInfo> getList();
--- Library-Management-System/src/main/java/com/lms/service/impl/BookInfoServiceImpl.java
+++ Library-Management-System/src/main/java/com/lms/service/impl/BookInfoServiceImpl.java
@@ -1,4 +1,24 @@
 package com.lms.service.impl;
 
-public class BookInfoServiceImpl {
+import com.lms.dao.BookInfoDao;
+import com.lms.entity.BookInfo;
+import com.lms.service.BookInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class BookInfoServiceImpl implements BookInfoService {
+
+    @Autowired
+    private BookInfoDao bookInfoDao;
+
+    @Override
+    public BookInfo getById(int bookId) {
+        return bookInfoDao.getBookId(bookId);
+    }
+
+    @Override
+    public List<BookInfo> getList() {
+        return bookInfoDao.selectBook(10);
+    }
 }
