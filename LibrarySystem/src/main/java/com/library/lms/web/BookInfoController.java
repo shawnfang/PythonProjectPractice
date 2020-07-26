24b3517cdc3ec60522bfbe4f87cc135168338cad
fangxiao <shawnfang07@gmail.com>
2020-07-26 20:29:53 +0800

    update

--- LibrarySystem/src/main/java/com/library/lms/web/BookInfoController.java
+++ LibrarySystem/src/main/java/com/library/lms/web/BookInfoController.java
@@ -3,22 +3,27 @@ package com.library.lms.web;
 import com.library.lms.pojo.BookInfo;
 import com.library.lms.service.BookInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class BookInfoController {
-
     @Autowired
     private BookInfoService bookInfoService;
 
     @GetMapping("/book")
-    private String list(Model model) {
-        List<BookInfo> list = bookInfoService.getList();
-        model.addAttribute("list", list);
-        return "list";
+    private BookInfo list() {
+        /*
+        BookInfo bookInfo = new BookInfo();
+        bookInfo.setBookId(1);
+        bookInfo.setBookName("test");
+        bookInfo.setBookPrice(10.0);
+
+         */
+        BookInfo bookInfos = bookInfoService.getById(1);
+        return bookInfos;
     }
 }
