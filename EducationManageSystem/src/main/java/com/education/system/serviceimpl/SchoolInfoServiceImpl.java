ec4bc12ea9e61002ac0b7ce9085de27127210fb4
shawnfang <shawnfang07@gmail.com>
2020-08-28 16:50:23 +0800

    update

---
+++ EducationManageSystem/src/main/java/com/education/system/serviceimpl/SchoolInfoServiceImpl.java
@@ -0,0 +1,43 @@
+package com.education.system.serviceimpl;
+
+import com.education.system.mapper.SchoolInfoMapper;
+import com.education.system.pojo.SchoolInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SchoolInfoServiceImpl {
+    @Autowired
+    private SchoolInfoMapper schoolInfoMapper;
+
+    private final static Logger logger = LoggerFactory.getLogger(SchoolInfoServiceImpl.class);
+
+    public String getSchoolInfoList(){
+        return schoolInfoMapper.schoolList();
+    }
+
+    public boolean addSchoolInfo(SchoolInfo schoolInfo){
+        boolean result = false;
+        schoolInfoMapper.addSchoolInfo(schoolInfo);
+        return result;
+    }
+
+    public boolean updateSchoolInfo(SchoolInfo schoolInfo){
+        boolean result = false;
+        return result;
+    }
+
+    public boolean deleteSchool(int id){
+        boolean result = false;
+        return result;
+    }
+
+    public String searchSchool(SchoolInfo schoolInfo){
+        return schoolInfoMapper.searchSchoolInfo(schoolInfo);
+    }
+
+}
--- EducationManageSystem/src/main/java/com/education/system/web/SchoolController.java
+++ EducationManageSystem/src/main/java/com/education/system/web/SchoolController.java
@@ -1,10 +1,53 @@
 package com.education.system.web;
 
 
+import com.education.system.serviceimpl.SchoolInfoServiceImpl;
+import com.education.system.util.ApiResult;
+import org.apache.ibatis.annotations.Mapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @RestController
 public class SchoolController {
+    @Autowired
+    private SchoolInfoServiceImpl schoolInfoService;
+    private final static Logger logger = LoggerFactory.getLogger(SchoolController.class);
 
+    @Autowired
+    private HttpServletRequest request;
+
+    @GetMapping("/delschool/{id}")
+    private ApiResult<?> delete(@PathVariable("id") int id){
+        boolean deleteResult = schoolInfoService.deleteSchool(id);
+        if (deleteResult) {
+            return ApiResult.newSuccess(true);
+        }else {
+            return ApiResult.newError("删除失败");
+        }
+    }
+
+    @GetMapping("/schooollist")
+    private ApiResult<?> schoollist(){
+        return ApiResult.newSuccess();
+    }
+
+    @PostMapping("/addschool")
+    private ApiResult<?> addschool(@RequestBody Map params){
+        return ApiResult.newSuccess();
+    }
+
+    @PostMapping("/updateschool")
+    private ApiResult<?> updateschool(@RequestBody Map params){
+        return ApiResult.newSuccess();
+    }
+
+    @PostMapping("searchschool")
+    private ApiResult<?> searchschool(@RequestBody Map params){
+        return ApiResult.newSuccess();
+    }
 }
