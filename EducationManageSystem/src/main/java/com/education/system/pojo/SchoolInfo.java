71a8f5e1404abdf405f2272e73ff9f4f02640941
shawnfang <shawnfang07@gmail.com>
2020-09-03 10:04:17 +0800

    update

--- EducationManageSystem/src/main/java/com/education/system/pojo/SchoolInfo.java
+++ EducationManageSystem/src/main/java/com/education/system/pojo/SchoolInfo.java
@@ -1,14 +1,17 @@
 package com.education.system.pojo;
 
+import lombok.Data;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
 public class SchoolInfo {
     private int id;
-    private String accout;
+    private String account;
     private String password;
     private String schoolName;
     private String schoolContactor;
     private String schoolPhone;
     private boolean status;
+
 }
--- EducationManageSystem/src/main/java/com/education/system/web/SchoolController.java
+++ EducationManageSystem/src/main/java/com/education/system/web/SchoolController.java
@@ -1,8 +1,11 @@
 package com.education.system.web;
 
 
+import com.education.system.pojo.SchoolInfo;
 import com.education.system.serviceimpl.SchoolInfoServiceImpl;
 import com.education.system.util.ApiResult;
+import com.education.system.util.PhoneFormatCheckUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.ibatis.annotations.Mapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,7 +41,58 @@ public class SchoolController {
 
     @PostMapping("/addschool")
     private ApiResult<?> addschool(@RequestBody Map params){
-        return ApiResult.newSuccess();
+        if(params.isEmpty()){
+            return ApiResult.newError("参数为空");
+        }
+        SchoolInfo schoolInfo = new SchoolInfo();
+        if (params.containsKey("schoolName")) {
+            if(params.get("schoolName") instanceof String){
+                if (params.get("schoolName").toString().length() <= 0 || params.get("schoolName").toString().length() > 20) {
+                    return ApiResult.newError("学校名称参数长度非法");
+                }
+                schoolInfo.setSchoolName((String)params.get("schoolName"));
+            }else {
+                return ApiResult.newError("学校名称参数类型错误");
+            }
+        }
+        if (params.containsKey("schoolContector")) {
+            if (params.get("schoolContector") instanceof String) {
+                if (params.get("schoolContector").toString().length() <= 0 || params.get("schoolContector").toString().length() > 10) {
+                    return ApiResult.newError("学校联系人参数长度非法");
+                }
+                schoolInfo.setSchoolContactor((String)params.get("schoolContector"));
+            }else {
+                return ApiResult.newError("学校联系人参数类型错误");
+            }
+        }
+        if (params.containsKey("schoolPhone")) {
+            if (!PhoneFormatCheckUtils.isChinaPhoneLegal((String)params.get("schoolPhone"))) {
+                return ApiResult.newError("电话号码参数非法");
+            }
+            schoolInfo.setSchoolPhone((String)params.get("schoolPhone"));
+        }
+        if (params.containsKey("account")) {
+            if (params.get("account") instanceof String) {
+                if (params.get("account").toString().length() <= 0 || params.get("account").toString().length() > 10) {
+                    return ApiResult.newError("学校账号参数长度非法");
+                }
+                schoolInfo.setAccount((String)params.get("account"));
+            }else {
+                return ApiResult.newError("学校账号参数类型错误");
+            }
+        }
+        if (params.containsKey("password")) {
+            if (params.get("password") instanceof String) {
+                if (params.get("password").toString().length() <= 0 || params.get("password").toString().length() > 10) {
+                    return ApiResult.newError("密码参数长度非法");
+                }
+                schoolInfo.setPassword((String)params.get("password"));
+            }else {
+                return ApiResult.newError("密码参数类型错误");
+            }
+        }
+        schoolInfo.setStatus(true);
+        return ApiResult.newSuccess(schoolInfoService.addSchoolInfo(schoolInfo));
     }
 
     @PostMapping("/updateschool")
